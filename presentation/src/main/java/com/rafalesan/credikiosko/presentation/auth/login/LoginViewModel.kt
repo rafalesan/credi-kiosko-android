c8367ea6e1563a9c95f76299c2c8f945e6e33ce4
Rafael Antonio Alegría Sánchez <rafael.alegria@golabstech.com>
2022-04-10 10:53:44 -0600

    Create presentation module for ui implementation and implement login screen design

---
+++ presentation/src/main/java/com/rafalesan/credikiosko/presentation/auth/login/LoginViewModel.kt
@@ -0,0 +1,9 @@
+package com.rafalesan.credikiosko.presentation.auth.login
+
+import com.rafalesan.credikiosko.presentation.base.BaseViewModel
+
+class LoginViewModel : BaseViewModel() {
+
+
+
+}
---
+++ presentation/src/main/java/com/rafalesan/credikiosko/presentation/base/BaseViewModel.kt
@@ -0,0 +1,12 @@
+package com.rafalesan.credikiosko.presentation.base
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.rafalesan.credikiosko.presentation.base.utils.SingleLiveEvent
+
+abstract class BaseViewModel : ViewModel() {
+
+    private val _toast = SingleLiveEvent<String>()
+    val toast: LiveData<String> = _toast
+
+}
