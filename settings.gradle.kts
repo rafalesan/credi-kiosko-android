ed6f1071f9bdfc61140df4bc757f01f6e3238b03
Rafael Antonio Alegría Sánchez <rafael.alegria@golabstech.com>
2022-04-12 16:40:01 -0600

    Use kotlin and android gradle plugin versions from versions.properties to have in a single place

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,6 +4,19 @@ pluginManagement {
         google()
         mavenCentral()
     }
+
+    //https://stackoverflow.com/questions/64644810/get-gradle-property-from-settings
+    val properties = File(rootDir, "versions.properties").inputStream().use {
+        java.util.Properties().apply { load(it) }
+    }
+    val kotlinVersion = properties.getValue("version.kotlin") as String
+    val androidGradleVersion = properties.getValue("plugin.android") as String
+
+    plugins {
+        id ("com.android.application") version androidGradleVersion apply false
+        id ("com.android.library") version androidGradleVersion apply false
+        id("org.jetbrains.kotlin.android") version kotlinVersion apply false
+    }
 }
 
 plugins {
