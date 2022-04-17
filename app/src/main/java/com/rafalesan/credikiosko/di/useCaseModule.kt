af7bfbaa796e152e9f565730bf139d185b64f577
Rafael Antonio Alegría Sánchez <rafael.alegria@golabstech.com>
2022-04-17 09:20:19 -0600

    Implement SignupUseCase and validate SignupData

--- app/src/main/java/com/rafalesan/credikiosko/di/useCaseModule.kt
+++ app/src/main/java/com/rafalesan/credikiosko/di/useCaseModule.kt
@@ -2,9 +2,11 @@ package com.rafalesan.credikiosko.di
 
 import com.rafalesan.credikiosko.domain.account.usecases.SaveUserSessionUseCase
 import com.rafalesan.credikiosko.domain.auth.usecases.LoginUseCase
+import com.rafalesan.credikiosko.domain.auth.usecases.SignupUseCase
 import org.koin.dsl.module
 
 val useCaseModule = module {
     single { LoginUseCase(get(), get()) }
+    single { SignupUseCase(get(), get()) }
     single { SaveUserSessionUseCase(get()) }
 }
