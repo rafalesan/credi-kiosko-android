31ff5917b3973f249608b94f943e5a888385b19c
Rafael Antonio Alegría Sánchez <rafael.alegria@golabstech.com>
2023-01-13 22:00:16 -0600

    Upgrade Gradle version to 7.5 and Gradle plugin to 7.4.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,6 +22,10 @@ pluginManagement {
 plugins {
     // See https://jmfayard.github.io/refreshVersions
     id("de.fayard.refreshVersions") version "0.40.2"
+////                            # available:"0.50.0"
+////                            # available:"0.50.1"
+////                            # available:"0.50.2"
+////                            # available:"0.51.0"
 }
 
 dependencyResolutionManagement {
