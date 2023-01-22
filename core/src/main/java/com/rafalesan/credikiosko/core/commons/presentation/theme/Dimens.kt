21e36b72a20ce5006ddce3420eedbca60496a029
Rafael Alegría <rafalesan96@gmail.com>
2023-01-22 00:00:53 -0600

    Refactor code to avoid hardcoding dimens

---
+++ core/src/main/java/com/rafalesan/credikiosko/core/commons/presentation/theme/Dimens.kt
@@ -0,0 +1,15 @@
+package com.rafalesan.credikiosko.core.commons.presentation.theme
+
+import androidx.compose.ui.unit.dp
+
+object Dimens {
+    val spaceDefault = 8.dp
+    val space2x = 16.dp
+    val space3x = 24.dp
+    val space4x = 32.dp
+    val space6x = 48.dp
+    val space8x = 64.dp
+    val space12units = 12.dp
+    val dialogBorderRadius = 8.dp
+    val iconDialogSize = 80.dp
+}
