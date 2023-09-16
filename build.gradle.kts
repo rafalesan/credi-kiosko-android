6b877dbc408372c827fb8783cfd26da61c28070a
Rafael Antonio Alegría Sánchez <rafalesan96@gmail.com>
2023-09-15 23:08:15 -0600

    Upgrade libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,8 +20,8 @@ fun BaseExtension.baseConfig() {
     }
 
     compileOptions.apply {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     tasks.withType<KotlinCompile> {
