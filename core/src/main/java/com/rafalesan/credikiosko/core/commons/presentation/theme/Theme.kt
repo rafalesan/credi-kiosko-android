e4dd2c1ffde6a8a46f07202004eb8a31cdb064aa
Rafael Alegría <rafalesan96@gmail.com>
2023-12-30 20:51:21 -0600

    Add dynamicColor feature for OS greater or equal than Android 12

--- core/src/main/java/com/rafalesan/credikiosko/core/commons/presentation/theme/Theme.kt
+++ core/src/main/java/com/rafalesan/credikiosko/core/commons/presentation/theme/Theme.kt
@@ -1,10 +1,14 @@
 package com.rafalesan.credikiosko.core.commons.presentation.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
 private val darkColorPalette = darkColorScheme(
     primary = Purple200,
@@ -30,16 +34,22 @@ private val lightColorPalette = lightColorScheme(
 @Composable
 fun CrediKioskoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colors = if (darkTheme) {
-        darkColorPalette
-    } else {
-        lightColorPalette
+
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> darkColorPalette
+        else -> lightColorPalette
     }
 
     MaterialTheme(
-        colorScheme = colors,
+        colorScheme = colorScheme,
         typography = Typography,
         shapes = Shapes,
         content = content
