addeca10ca9cb4735c9aa0af9290b2eed7c3b1c0
Rafael Alegría <rafalesan96@gmail.com>
2024-02-02 19:25:57 -0600

    Create blank welcome screen

--- app/src/main/java/com/rafalesan/credikiosko/main/FullComposeMainActivity.kt
+++ app/src/main/java/com/rafalesan/credikiosko/main/FullComposeMainActivity.kt
@@ -8,23 +8,15 @@ import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
-import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.rafalesan.credikiosko.auth.AuthViewModel
-import com.rafalesan.credikiosko.auth.login.LoginScreenNavCompose
-import com.rafalesan.credikiosko.auth.signup.SignUpScreenNavCompose
 import com.rafalesan.credikiosko.core.commons.emptyString
 import com.rafalesan.credikiosko.core.commons.presentation.theme.CrediKioskoTheme
 import com.rafalesan.credikiosko.home.HomeScreenNavCompose
+import com.rafalesan.credikiosko.onboarding.presentation.WelcomeScreen
 import com.rafalesan.products.presentation.product_form.ProductFormScreen
 import com.rafalesan.products.presentation.products_list.ProductsScreen
 import dagger.hilt.android.AndroidEntryPoint
@@ -36,42 +28,20 @@ class FullComposeMainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CrediKioskoTheme {
-                val authViewModel: AuthViewModel = hiltViewModel()
                 val navController = rememberNavController()
-
-                val existUserSession by authViewModel.existUserSessionFlow.collectAsState()
-                val startDestination by remember {
-                    derivedStateOf {
-                        if (existUserSession) {
-                            "home"
-                        } else {
-                            "auth"
-                        }
-                    }
-                }
 
                 NavHost(
                     navController = navController,
-                    startDestination = startDestination,
+                    startDestination = "welcome",
                     enterTransition = { fadeIn(animationSpec = tween()) },
                     exitTransition = { ExitTransition.None },
                     popEnterTransition = { EnterTransition.None },
                     popExitTransition = { fadeOut(animationSpec = tween()) }
                 ) {
 
-                    navigation(
-                        route = "auth",
-                        startDestination = "login"
-                    ) {
-                        composable("login") {
-                            LoginScreenNavCompose(navController)
-                        }
-
-                        composable("sign_up") {
-                            SignUpScreenNavCompose()
-                        }
+                    composable("welcome") {
+                        WelcomeScreen(navController)
                     }
-
 
                     composable("home") {
                         HomeScreenNavCompose(navController = navController)
